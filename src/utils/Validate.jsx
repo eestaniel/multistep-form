4277dde8ee9547a92529d349694bd044d2c11c93
EEstaniel <edwineestaniel93@gmail.com>
2023-05-30 20:17:52 -0700

    fix: handle validation for input based on blur or button

--- src/utils/Validate.jsx
+++ src/utils/Validate.jsx
@@ -4,32 +4,106 @@ const Validate = (formValues) => {
     let errors = {}
     // handle on blur
     if (formValues.type === 'blur') {
-        // handle empty input
-        if (!formValues.target.value.trim()) {
-            errors[formValues.target.id] = `${formValues.target.id} required`
-        }
-        // handle mismatch pattern based on
-        else if (formValues.target.validity.patternMismatch) {
-            if (formValues.target.id === 'name') {
-                errors[formValues.target.id] = 'Name must contain only letters'
-            } else if (formValues.target.id === 'email') {
-                errors[formValues.target.id] = 'Must be a valid email address'
-            } else if (formValues.target.id === 'phone') {
-                errors[formValues.target.id] = 'Must be a valid phone number'
-            }
+        const id = formValues.target.id
+
+        switch (id) {
+            // handle validation for name
+            case 'name':
+                // handle empty input
+                if (!formValues.target.value.trim()) {
+                    errors.name = 'Name required'
+                }
+                // handle mismatch pattern
+                else if (formValues.target.value.match(/[^a-zA-Z ]+/g)) {
+                    errors.name = 'Name must contain only letters'
+                }
+                // handle full name
+                else {
+                    let name = formValues.target.value.trim().split(' ')
+                    if (name.length < 2) {
+                        errors.name = 'Full name required'
+                    }
+                }
+                break;
+            // handle validation for email
+            case 'email':
+                // handle empty input
+                if (!formValues.target.value.trim()) {
+                    errors.email = 'Email required'
+                }
+                // handle mismatch pattern
+                else {
+                    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+                    if (!emailPattern.test(formValues.target.value)) {
+                        errors.email = 'Must be a valid email address';
+                    }
+                }
+                break;
+            // handle validation for phone
+            case 'phone':
+                // handle empty input
+                if (!formValues.target.value.trim()) {
+                    errors.phone = 'Phone number required'
+                }
+                // handle mismatch pattern
+                else {
+                    const phonePattern = /^\+?1?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})$/;
+                    if (!phonePattern.test(formValues.target.value)) {
+                        errors.phone = 'Must be a valid phone number';
+                    }
+                }
+                break;
+
         }
     }
+
+
+
+
     // handle button click
     else {
+
+        // handle validation for name
+        //handle empty input
         if (!formValues.name.trim()) {
             errors.name = 'Name required'
         }
+        // handle mismatch pattern
+        else if (formValues.name.match(/[^a-zA-Z ]+/g)) {
+            errors.name = 'Name must contain only letters'
+        }
+        // handle full name
+        else {
+            let name = formValues.name.trim().split(' ')
+            if (name.length < 2) {
+                errors.name = 'Full name required'
+            }
+        }
+
+        // handle validation for email
+        // handle empty input
         if (!formValues.email.trim()) {
             errors.email = 'Email required'
         }
-        if (!formValues.phone.trim()) {
-            errors.phone = 'Phone required'
+        // handle mismatch pattern
+        else {
+            const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+            if (!emailPattern.test(formValues.email)) {
+                errors.email = 'Must be a valid email address';
+            }
         }
+
+        // handle validation for phone
+        // handle empty input
+        if (!formValues.phone.trim()) {
+            errors.phone = 'Phone number required'
+        } else {
+            const phoneNumberPattern = /^\+?1?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})$/;
+            if (!phoneNumberPattern.test(formValues.phone)) {
+                errors[formValues.target.id] = 'Must be a valid phone number'
+            }
+        }
+
     }
 
 
