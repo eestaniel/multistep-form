da959f4b2a3626ba60b78a7f07f48828efb20439
EEstaniel <edwineestaniel93@gmail.com>
2023-05-31 19:15:54 -0700

    feat: add css mobile version addon

--- src/components/pages/Addons.jsx
+++ src/components/pages/Addons.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {useStateContext} from "../../context/StateProvider.jsx";
-import DirectionButton from "../form/DirectionButton.jsx";
+import './styles/Addons.css'
 
 
 const Addons = () => {
     const {planDuration} = useStateContext();
-    const {setAddonBilling} = useStateContext();
+    const {addonBilling, setAddonBilling} = useStateContext();
     const {isChecked, setIsChecked} = useStateContext();
 
     const handleCheck = (e) => {
@@ -13,15 +13,30 @@ const Addons = () => {
 
         setIsChecked((prevIsChecked) => ({
             ...prevIsChecked,
-            [id]: checked
+            [id]: checked,
         }));
 
         setAddonBilling((prevAddonBilling) => ({
             ...prevAddonBilling,
             [id]: {
                 ...prevAddonBilling[id],
-                enable: checked
-            }
+                enable: checked,
+            },
+        }));
+    };
+
+    const handleParentClick = (id) => {
+        setIsChecked((prevIsChecked) => ({
+            ...prevIsChecked,
+            [id]: !prevIsChecked[id], // Toggle the checked state
+        }));
+
+        setAddonBilling((prevAddonBilling) => ({
+            ...prevAddonBilling,
+            [id]: {
+                ...prevAddonBilling[id],
+                enable: !prevAddonBilling[id].enable, // Toggle the enable state
+            },
         }));
     };
 
@@ -32,45 +47,58 @@ const Addons = () => {
                 <h2 className="addon__subheader">Add-ons help enhance your gaming experience</h2>
 
                 <div className="addon__group-container">
-                    <div className="addon__group__item">
+                    <div
+                        className={`addon__group__item ${addonBilling['online-service'].enable ? 'addon__group__item--active' : ''}`}
+                         onClick={() => handleParentClick('online-service')
+                    }
+                    >
                         <input type="checkbox"
                                id={'online-service'}
                                checked={isChecked['online-service']}
                                onChange={handleCheck}
                         />
-                        <h3 className="addon__group__item__header">Online Services</h3>
+                        <div className="addon__group__item__subgroup">
+                            <h3 className="addon__group__item__header">Online service</h3>
+                            <p>Access to multiplayer games</p>
+                        </div>
                         <p className="price">{
-                            planDuration === 'monthly' ? '+$1/month' : '+$10/year'
+                            planDuration === 'monthly' ? '+$1/mo' : '+$10/yr'
                         }</p>
+
                     </div>
-                    <div className="addon__group__item">
+                    <div
+                        className={`addon__group__item ${addonBilling['larger-storage'].enable ? 'addon__group__item--active' : ''}`}
+                        onClick={() => handleParentClick('larger-storage')}>
                         <input type="checkbox"
                                id={'larger-storage'}
                                checked={isChecked['larger-storage']}
                                onChange={handleCheck}
                         />
-                        <h3 className="addon__group__item__header">Larger storage</h3>
+                        <div className="addon__group__item__subgroup">
+                            <h3 className="addon__group__item__header">Larger storage</h3>
+                            <p>Extra 1TB of cloud save</p>
+                        </div>
                         <p className="price">{
-                            planDuration === 'monthly' ? '+$2/month' : '+$20/year'
+                            planDuration === 'monthly' ? '+$2/mo' : '+$20/yr'
                         }</p>
                     </div>
-                    <div className="addon__group__item">
+                    <div
+                        className={`addon__group__item ${addonBilling['customizable-profile'].enable ? 'addon__group__item--active' : ''}`}
+                        onClick={() => handleParentClick('customizable-profile')}>
                         <input type="checkbox"
                                id={'customizable-profile'}
                                checked={isChecked['customizable-profile']}
                                onChange={handleCheck}
                         />
-                        <h3 className="addon__group__item__header">Customizable profile</h3>
+                        <div className="addon__group__item__subgroup">
+                            <h3 className="addon__group__item__header">Customizable profile</h3>
+                            <p>Custom theme on your profile</p>
+                        </div>
                         <p className="price">{
-                            planDuration === 'monthly' ? '+$2/month' : '+$20/year'
+                            planDuration === 'monthly' ? '+$2/mo' : '+$20/yr'
                         }</p>
                     </div>
                 </div>
-            </div>
-
-            <div className="form__plan__button-group">
-                <DirectionButton id={'go-back'} text={'Go Back'}/>
-                <DirectionButton id={'continue'} text={'Continue'}/>
             </div>
         </React.Fragment>
 
